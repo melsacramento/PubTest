9807bcf40d384ccb69388acf8bcf2748d54dfe61
melsacramento <88633931+melsacramento@users.noreply.github.com>
2022-02-11 13:13:40 +1300

    Update edge_agent.sh

--- deployment/edge_agent.sh
+++ deployment/edge_agent.sh
@@ -62,16 +62,16 @@ main() {
   curl -L https://raw.githubusercontent.com/melsacramento/PubTest/main/deployment/portainer-agent-edge-k8s.yaml -o portainer-agent-edge-k8s.yaml || errorAndExit "Unable to download agent manifest"
 
   info "Creating Portainer namespace..."
-  kubectl create namespace portainer
+  microk8s kubectl create namespace portainer
 
   info "Creating agent configuration..."
-  kubectl create configmap portainer-agent-edge --from-literal="edge.id=$EDGE_ID" --from-literal="edge.insecure_poll=$EDGE_INSECURE_POLL"  -n portainer
+  microk8s kubectl create configmap portainer-agent-edge --from-literal="edge.id=$EDGE_ID" --from-literal="edge.insecure_poll=$EDGE_INSECURE_POLL"  -n portainer
 
   info "Creating agent secret..."
-  kubectl create secret generic portainer-agent-edge-key "--from-literal=edge.key=$EDGE_KEY" -n portainer
+  microk8s kubectl create secret generic portainer-agent-edge-key "--from-literal=edge.key=$EDGE_KEY" -n portainer
 
   info "Deploying agent..."
-  kubectl apply -f portainer-agent-edge-k8s.yaml || errorAndExit "Unable to deploy agent manifest"
+  microk8s kubectl apply -f portainer-agent-edge-k8s.yaml || errorAndExit "Unable to deploy agent manifest"
 
   success "Portainer Edge agent successfully deployed"
   exit 0
